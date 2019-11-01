28dc4346b7e87d58ef3bdac649c1aad44f3eee4e
ivan <ivan@rodextravel.tours>
2019-11-01 13:46:08 +0700

    update cache

--- tt_webservice/views/tt_webservice_views.py
+++ tt_webservice/views/tt_webservice_views.py
@@ -1,12 +1,10 @@
 import json
 
 def get_cache_version():
-    file = open(var_log_path()+"javascript_version.txt", "r")
-    for idx, line in enumerate(file):
-        if idx == 0:
-            javascript_version = line.split('\n')[0]
+    file = open(var_log_path()+"cache_version.txt", "r")
+    cache_version = file.read()
     file.close()
-    return javascript_version
+    return cache_version
 
 def get_cache_data(javascript_version):
     file = open(var_log_path()+'version' + str(javascript_version) + ".txt", "r")
