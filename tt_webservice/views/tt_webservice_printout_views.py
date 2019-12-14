a18cec20d8fa2d10a4489be9f593e29a6d93fbfb
ivan <ivan@rodextravel.tours>
2019-12-14 14:56:37 +0700

    update_airline_webservice_payment

---
+++ tt_webservice/views/tt_webservice_printout_views.py
@@ -0,0 +1,75 @@
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+from tools import util, ERR
+from tools.parser import *
+from datetime import *
+from tools.parser import *
+from ..static.tt_webservice.url import *
+import json
+import logging
+import traceback
+from .tt_webservice_views import *
+_logger = logging.getLogger(__name__)
+
+month = {
+    'Jan': '01',
+    'Feb': '02',
+    'Mar': '03',
+    'Apr': '04',
+    'May': '05',
+    'Jun': '06',
+    'Jul': '07',
+    'Aug': '08',
+    'Sep': '09',
+    'Oct': '10',
+    'Nov': '11',
+    'Dec': '12',
+    '01': 'Jan',
+    '02': 'Feb',
+    '03': 'Mar',
+    '04': 'Apr',
+    '05': 'May',
+    '06': 'Jun',
+    '07': 'Jul',
+    '08': 'Aug',
+    '09': 'Sep',
+    '10': 'Oct',
+    '11': 'Nov',
+    '12': 'Dec',
+}
+
+@api_view(['GET', 'POST'])
+def api_models(request):
+    try:
+        req_data = util.get_api_request_data(request)
+        if req_data['action'] == 'get_printout':
+            res = get_printout(request)
+        else:
+            res = ERR.get_error_api(1001)
+    except Exception as e:
+        res = ERR.get_error_api(500, additional_message=str(e))
+    return Response(res)
+
+def get_printout(request):
+    try:
+        data = {
+            'order_number': request.POST['order_number'],
+            'mode': request.POST['mode'],
+            'provider_type': request.POST['provider_type'],
+            'bill_to_name': request.POST['bill_to_name'],
+        }
+        headers = {
+            "Accept": "application/json,text/html,application/xml",
+            "Content-Type": "application/json",
+            "action": "get_printout_api",
+            "signature": request.POST['signature'],
+        }
+    except Exception as e:
+        _logger.error(msg=str(e) + '\n' + traceback.format_exc())
+    i
+    res = util.send_request(url=url + 'printout', data=data, headers=headers, method='POST')
+
+    if res['result']['error_code'] == 0:
+        pass
+    return res
