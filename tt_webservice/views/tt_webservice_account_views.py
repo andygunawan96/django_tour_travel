696ce9c682a216519da0f554698c033c196a719d
Joshua <it@rodextravel.tours>
2020-01-28 14:30:20 +0700

    ubah import datetime di account views

--- tt_webservice/views/tt_webservice_account_views.py
+++ tt_webservice/views/tt_webservice_account_views.py
@@ -3,7 +3,7 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from tools import util, ERR
 from tools.parser import *
-import datetime
+from datetime import datetime
 from ..static.tt_webservice.url import *
 import json
 import logging
