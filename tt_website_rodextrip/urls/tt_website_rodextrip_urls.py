6344a9301466b97e0f59b6ef347e728241d65377
ivan <ivan@rodextravel.tours>
2020-01-31 08:19:16 +0000

    update_url_django2.2.6

--- tt_website_rodextrip/urls/tt_website_rodextrip_urls.py
+++ tt_website_rodextrip/urls/tt_website_rodextrip_urls.py
@@ -1,12 +1,12 @@
 from .tt_website_rodextrip_header_urls import *
 from ..views import tt_website_rodextrip_views as view
 
-urlpatterns.append(re_path('(?i)reservation', view.reservation, name="reservation"))
-urlpatterns.append(re_path('(?i)page_admin', view.admin, name="admin"))
-urlpatterns.append(re_path('(?i)payment', view.payment, name="payment"))
-urlpatterns.append(re_path('(?i)testing', view.testing, name="testing"))
-urlpatterns.append(re_path('(?i)top_up/history', view.top_up_history, name="top_up_history"))
-urlpatterns.append(re_path('(?i)top_up', view.top_up, name="top_up"))
-urlpatterns.append(re_path('(?i)dashboard', view.index, name="index"))
-urlpatterns.append(re_path('(?i)', view.login, name="login"))
+urlpatterns.append(re_path('reservation', view.reservation, name="reservation"))
+urlpatterns.append(re_path('page_admin', view.admin, name="admin"))
+urlpatterns.append(re_path('payment', view.payment, name="payment"))
+urlpatterns.append(re_path('testing', view.testing, name="testing"))
+urlpatterns.append(re_path('top_up/history', view.top_up_history, name="top_up_history"))
+urlpatterns.append(re_path('top_up', view.top_up, name="top_up"))
+urlpatterns.append(re_path('dashboard', view.index, name="index"))
+urlpatterns.append(re_path('', view.login, name="login"))
 # (?i) buat upper case to lower case bisa detect walaupun uppercase
