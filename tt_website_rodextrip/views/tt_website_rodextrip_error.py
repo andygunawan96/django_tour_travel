24e6206b7cf6e02f860506b3e57eb2e9f4bdad16
ivan <ivan@rodextravel.tours>
2020-02-18 04:59:41 +0000

    Update logger new

--- tt_website_rodextrip/views/tt_website_rodextrip_error.py
+++ tt_website_rodextrip/views/tt_website_rodextrip_error.py
@@ -3,12 +3,36 @@ from .tt_website_rodextrip_views import *
 MODEL_NAME = 'tt_website_rodextrip'
 
 def handler400(request, *args, **argv):
+    values = get_data_template(request, 'login')
+    javascript_version = get_javascript_version()
+    values.update({
+        'static_path': path_util.get_static_path(MODEL_NAME),
+        'javascript_version': javascript_version,
+        'static_path_url_server': get_url_static_path(),
+        'username': request.session.get('user_account'),
+    })
     return render(request, MODEL_NAME + '/error/404.html', {})
 
 def handler403(request, *args, **argv):
+    values = get_data_template(request, 'login')
+    javascript_version = get_javascript_version()
+    values.update({
+        'static_path': path_util.get_static_path(MODEL_NAME),
+        'javascript_version': javascript_version,
+        'static_path_url_server': get_url_static_path(),
+        'username': request.session.get('user_account'),
+    })
     return render(request, MODEL_NAME + '/error/404.html', {})
 
 def handler404(request, *args, **argv):
+    values = get_data_template(request, 'login')
+    javascript_version = get_javascript_version()
+    values.update({
+        'static_path': path_util.get_static_path(MODEL_NAME),
+        'javascript_version': javascript_version,
+        'static_path_url_server': get_url_static_path(),
+        'username': request.session.get('user_account'),
+    })
     return render(request, MODEL_NAME + '/error/404.html', {})
 
 def handler500(request, *args, **argv):
