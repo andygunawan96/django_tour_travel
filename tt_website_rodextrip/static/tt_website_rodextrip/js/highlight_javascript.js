4b892085e27ab081eb4f868185de5dc87757d195
ivan <ivan@rodextravel.tours>
2020-03-06 03:13:41 +0000

    Update highlight show

--- tt_website_rodextrip/static/tt_website_rodextrip/js/highlight_javascript.js
+++ tt_website_rodextrip/static/tt_website_rodextrip/js/highlight_javascript.js
@@ -8,12 +8,17 @@ function add_table_of_highlight(data){
             <div class="col-lg-1 col-md-1 col-sm-1" style="color:`+text_color+`;padding-top:10px;">
                 `+counter_highlight+`
             </div>
-            <div class="col-lg-10 col-md-10 col-sm-10">
+            <div class="col-lg-4 col-md-4 col-sm-4">
                 <div class="input-container-search-ticket">
-                    <input type="text" class="form-control" name="highlight_url`+counter_highlight+`" id="highlight_url`+counter_highlight+`" placeholder="Highlight URL " onfocus="this.placeholder = ''" onblur="this.placeholder = 'Highlight URL '" value="`+data+`">
+                    <input type="text" class="form-control" name="highlight_title`+counter_highlight+`" id="highlight_title`+counter_highlight+`" placeholder="Title " onfocus="this.placeholder = ''" onblur="this.placeholder = 'Title '" value="`+data[0]+`">
                 </div>
             </div>
-            <div class="col-lg-1 col-md-1 col-sm-1" style="color:`+text_color+`;padding-top:10px;">
+            <div class="col-lg-6 col-md-6 col-sm-6">
+                <div class="input-container-search-ticket">
+                    <input type="text" class="form-control" name="highlight_url`+counter_highlight+`" id="highlight_url`+counter_highlight+`" placeholder="Highlight URL " onfocus="this.placeholder = ''" onblur="this.placeholder = 'Highlight URL '" value="`+data[1]+`">
+                </div>
+            </div>
+            <div class="col-lg-1 col-md-1 col-sm-1" style="color:`+text_color+`;">
                 <button type="button" class="primary-delete-date" onclick="delete_table_of_highlight(`+counter_highlight+`)">
                     <i class="fa fa-trash-alt" style="color:#E92B2B;font-size:20px;"></i>
                 </button>
@@ -33,7 +38,7 @@ function delete_table_of_highlight(val){
     }
 }
 
-function get_highlight(){
+function get_highlight(type){
     $.ajax({
        type: "POST",
        url: "/webservice/account",
@@ -42,8 +47,20 @@ function get_highlight(){
        },
        data: {},
        success: function(msg) {
-            for(i in msg){
-                add_table_of_highlight(msg[i]);
+            if(type == 'setting'){
+                document.getElementById("highlight_div").innerHTML = '';
+                counter_highlight = 0;
+                for(i in msg){
+                    add_table_of_highlight(msg[i]);
+                }
+            }else if(type == 'header'){
+                text = '';
+                for(i in msg){
+                    text += `<li><a href="`+msg[i][1]+`" target="_blank">`+msg[i][0]+`</a></li>`;
+                }
+                if(msg.length == 0)
+                    text += `<li>No Highlight</li>`;
+                document.getElementById('highlight_header_div').innerHTML = text;
             }
        },
        error: function(XMLHttpRequest, textStatus, errorThrown) {
@@ -59,7 +76,9 @@ function get_highlight(){
 function save_highlight(){
     data = [];
     for(i=1;i<=counter_highlight;i++){
-        data.push(document.getElementById('highlight_url'+i).value);
+        try{
+            data.push([document.getElementById('highlight_title'+i).value,document.getElementById('highlight_url'+i).value]);
+        }catch(err){}
     }
     console.log(data);
     $.ajax({
@@ -72,6 +91,8 @@ function save_highlight(){
             'data': JSON.stringify(data)
        },
        success: function(msg) {
+            get_highlight('setting');
+            get_highlight('header');
             Swal.fire({
               type: 'success',
               title: 'Update',
