8e109c59385fdfe99637aed0375afddc168a1664
rodex-it-08 <cenius@rodextravel.tours>
2020-03-13 09:46:28 +0700

    owl carousel login

---
+++ tt_website_rodextrip/static/tt_website_rodextrip/js/vendor/login_main.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+
+    $('.owl-carousel-login').owlCarousel({
+        loop:true,
+        nav: false,
+        navRewind:false,
+        rewind: false,
+        margin: 20,
+        items:1,
+        responsiveClass:true,
+        dots: true,
+        merge: false,
+        lazyLoad:true,
+        smartSpeed:500,
+        autoplay: true,
+        autoplayTimeout:10000,
+        autoplayHoverPause:false,
+        navText: ['<i class="fa fa-chevron-left owl-wh"/>', '<i class="fa fa-chevron-right owl-wh"/>'],
+        responsive:{
+            0:{
+                items:1,
+                nav:true
+            },
+            480:{
+                items:1,
+                nav:true
+            },
+            768:{
+                items:3,
+                nav:false
+            },
+            961:{
+                items:3,
+                nav:true,
+            }
+        }
+    });
+
+});
