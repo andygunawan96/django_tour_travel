5a7947853aaf559fb1535659e8fc2b519f43f02f
ivan <ivan@rodextravel.tours>
2020-12-15 10:54:56 +0700

    update_selection_report

--- tt_website_rodextrip/static/tt_website_rodextrip/js/webservice_report.js
+++ tt_website_rodextrip/static/tt_website_rodextrip/js/webservice_report.js
@@ -1,16 +1,28 @@
+date_day = {
+    'Sunday': 0,
+    'Monday': 1,
+    'Tuesday': 2,
+    'Wednesday': 3,
+    'Thursday': 4,
+    'Friday': 5,
+    'Saturday': 6
+}
 $(document).ready(function(){
     result_data = [];
-
+    var now = moment().format('dddd DD');
     $('#date_report').daterangepicker({
         "showDropdowns": true,
         ranges: {
             'Today': [moment(), moment()],
             'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
-            'Last 7 Days': [moment().subtract(6, 'days'), moment()],
-            'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+            'This Week': [moment().subtract(date_day[now.split(' ')[0]], 'days'), moment()],
+            'Last Week': [moment().subtract(date_day[now.split(' ')[0]]+7, 'days'), moment().subtract(date_day[now.split(' ')[0]]+1, 'days')],
             'This Month': [moment().startOf('month'), moment().endOf('month')],
             'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+            'Last 7 Days': [moment().subtract(6, 'days'), moment()],
+            'Last 30 Days': [moment().subtract(29, 'days'), moment()],
         },
+        "alwaysShowCalendars": true,
         "startDate": moment().startOf('month'),
         "endDate": moment().endOf('month'),
         "maxDate": moment().endOf('month'),
