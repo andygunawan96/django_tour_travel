2ce6dcd3b7c61a20207a188bb1c1a9a2556c1de4
rodex-it-08 <cenius@rodextravel.tours>
2021-02-18 16:58:45 +0700

    max-height style jadi 350px

--- tt_website_rodextrip/static/tt_website_rodextrip/js/webservice_content.js
+++ tt_website_rodextrip/static/tt_website_rodextrip/js/webservice_content.js
@@ -793,7 +793,7 @@ function get_page(data){
                 msg.result.response.body = msg.result.response.body.replace(/&nbsp;/g,' ');
                 document.getElementById('container').innerHTML = msg.result.response.body;
                 document.getElementById('title').innerHTML = `<h1 style="text-align:center;color:`+color+`">`+msg.result.response.title+`</h1>`;
-                document.getElementById('header_page').innerHTML = `<img src="`+msg.result.response.image_carousel+`" style="max-height:400px;max-width:100%;" alt="`+msg.result.response.title+`" title="" />`;
+                document.getElementById('header_page').innerHTML = `<img src="`+msg.result.response.image_carousel+`" style="max-height:350px;max-width:100%;" alt="`+msg.result.response.title+`" title="" />`;
             }else{
                 document.getElementById('container').innerHTML = 'Page not found';
             }
