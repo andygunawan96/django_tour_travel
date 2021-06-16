a7713d216a5e02be6c5355ae6b9ec881829c789d
ivan <ivan@rodextravel.tours>
2021-06-16 08:07:43 +0000

    Update path url

--- tt_website_rodextrip/urls/tt_website_rodextrip_medical_urls.py
+++ tt_website_rodextrip/urls/tt_website_rodextrip_medical_urls.py
@@ -4,5 +4,6 @@ from ..views import tt_website_rodextrip_medical_views as view
 
 urlpatterns.append(path('<path:vendor>/booking/<path:order_number>', view.booking, name="medical_booking"))
 urlpatterns.append(path('<path:vendor>/passenger/<path:test_type>', view.passenger, name="medical_passenger"))
+urlpatterns.append(path('medical/<path:vendor>/review', view.review, name="medical_review"))
 urlpatterns.append(path('medical/<path:vendor>', view.medical, name="medical"))
-urlpatterns.append(re_path('medical/review', view.review, name="medical_review"))
+
