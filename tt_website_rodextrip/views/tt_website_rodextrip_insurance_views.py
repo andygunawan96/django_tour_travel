7ea87e4ad395237b87187976a0a5d53fa3f76f70
ivan <ivan@rodextravel.tours>
2021-11-11 10:22:30 +0700

    update_relation

--- tt_website_rodextrip/views/tt_website_rodextrip_insurance_views.py
+++ tt_website_rodextrip/views/tt_website_rodextrip_insurance_views.py
@@ -319,7 +319,7 @@ def review(request):
                         "identity_number": request.POST['adult_relation1_passport_number' + str(i + 1)],
                         "identity_expdate": request.POST['adult_relation1_passport_expired_date' + str(i + 1)],
                         "identity_country_of_issued_name": request.POST['adult_relation1_passport_country_of_issued' + str(i + 1) + '_id'],
-                        "relation": request.POST['adult_relation1_relation' + str(i + 1) + '_id'],
+                        "relation": request.POST['adult_relation1_relation' + str(i + 1)],
                     })
 
                 if request.POST['adult_relation2_first_name' + str(i + 1)]:
@@ -333,7 +333,7 @@ def review(request):
                         "identity_number": request.POST['adult_relation2_passport_number' + str(i + 1)],
                         "identity_expdate": request.POST['adult_relation2_passport_expired_date' + str(i + 1)],
                         "identity_country_of_issued_name": request.POST['adult_relation2_passport_country_of_issued' + str(i + 1) + '_id'],
-                        "relation": request.POST['adult_relation2_relation' + str(i + 1) + '_id'],
+                        "relation": request.POST['adult_relation2_relation' + str(i + 1)],
                     })
 
                 if request.POST['adult_relation3_first_name' + str(i + 1)]:
@@ -347,7 +347,7 @@ def review(request):
                         "identity_number": request.POST['adult_relation3_passport_number' + str(i + 1)],
                         "identity_expdate": request.POST['adult_relation3_passport_expired_date' + str(i + 1)],
                         "identity_country_of_issued_name": request.POST['adult_relation3_passport_country_of_issued' + str(i + 1) + '_id'],
-                        "relation": request.POST['adult_relation3_relation' + str(i + 1) + '_id'],
+                        "relation": request.POST['adult_relation3_relation' + str(i + 1)],
                     })
 
                 if request.POST['adult_relation4_first_name' + str(i + 1)]:
@@ -361,7 +361,7 @@ def review(request):
                         "identity_number": request.POST['adult_relation4_passport_number' + str(i + 1)],
                         "identity_expdate": request.POST['adult_relation4_passport_expired_date' + str(i + 1)],
                         "identity_country_of_issued_name": request.POST['adult_relation4_passport_country_of_issued' + str(i + 1) + '_id'],
-                        "relation": request.POST['adult_relation4_relation' + str(i + 1) + '_id'],
+                        "relation": request.POST['adult_relation4_relation' + str(i + 1)],
                     })
                 if request.POST['adult_relation5_first_name' + str(i + 1)]:
                     ahli_waris.update({
@@ -374,7 +374,7 @@ def review(request):
                         "identity_number": request.POST['adult_relation5_passport_number' + str(i + 1)],
                         "identity_expdate": request.POST['adult_relation5_passport_expired_date' + str(i + 1)],
                         "identity_country_of_issued_name": request.POST['adult_relation5_passport_country_of_issued' + str(i + 1) + '_id'],
-                        "relation": request.POST['adult_relation5_relation' + str(i + 1) + '_id'],
+                        "relation": request.POST['adult_relation5_relation' + str(i + 1)],
                     })
 
                 adult.append({
