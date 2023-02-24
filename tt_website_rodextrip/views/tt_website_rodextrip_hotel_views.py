5fe7f6d566502070eab6e016b7574a53dc856ce0
rodex-it-08 <cenius@rodextravel.tours>
2023-02-24 15:25:35 +0700

    check banner untuk tiap produk views, single page tampilan airline, activity, hotel, ppob, train

--- tt_website_rodextrip/views/tt_website_rodextrip_hotel_views.py
+++ tt_website_rodextrip/views/tt_website_rodextrip_hotel_views.py
@@ -80,8 +80,8 @@ def hotel(request):
                 'javascript_version': javascript_version,
                 'update_data': 'false',
                 'static_path_url_server': get_url_static_path(),
-                'big_banner_value': check_big_banner(),
-                'small_banner_value': check_small_banner(),
+                'big_banner_value': check_big_banner('hotel'),
+                'small_banner_value': check_small_banner('hotel'),
                 'dynamic_page_value': check_dynamic_page(),
                 'signature': request.session['signature'],
             })
--- tt_website_rodextrip/views/tt_website_rodextrip_tour_views.py
+++ tt_website_rodextrip/views/tt_website_rodextrip_tour_views.py
@@ -88,8 +88,8 @@ def tour(request):
                 'javascript_version': javascript_version,
                 'update_data': 'false',
                 'static_path_url_server': get_url_static_path(),
-                'big_banner_value': check_big_banner(),
-                'small_banner_value': check_small_banner(),
+                'big_banner_value': check_big_banner('tour'),
+                'small_banner_value': check_small_banner('tour'),
                 'dynamic_page_value': check_dynamic_page(),
                 'signature': request.session['signature'],
 
