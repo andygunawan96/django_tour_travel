1ba36a2c21f7a6cd1f54e5cca6e46d5fcd83436b
rodex-it-08 <cenius@rodextravel.tours>
2023-06-08 15:20:33 +0700

    delete text class of service AdanB

--- tt_website/static/tt_website/js/airline_javascript.js
+++ tt_website/static/tt_website/js/airline_javascript.js
@@ -3391,7 +3391,7 @@ function sort(){
                                                             <div style="background:white; padding:15px; border:1px solid #cdcdcd; overflow-y:auto; max-height:200px;">
                                                                <div class="row">
                                                                    <div class="col-lg-12">
-                                                                        <h6>Class Of ServiceA</h6>
+                                                                        <h6>Class Of Service</h6>
                                                                         <hr/>
                                                                    </div>`;
 
@@ -4805,7 +4805,7 @@ function airline_pick_mc(type){
                                             <div style="background:white; padding:15px; border:1px solid #cdcdcd; overflow-y:auto; max-height:200px;">
                                                <div class="row">
                                                    <div class="col-lg-12">
-                                                        <h6>Class Of ServiceB</h6>
+                                                        <h6>Class Of Service</h6>
                                                         <hr/>
                                                    </div>`;
                                                    for(k in airline_pick_list[i].segments[j].fares){
