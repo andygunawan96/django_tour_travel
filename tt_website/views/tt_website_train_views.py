337c2e229d3785c3fcfd5c558de06a9721c2e9f0
Andy Gunawan <it@rodextravel.tours>
2023-07-11 08:46:33 +0700

    train_search_corporate_mode

--- tt_website/views/tt_website_train_views.py
+++ tt_website/views/tt_website_train_views.py
@@ -155,6 +155,19 @@ def search(request):
             if translation.LANGUAGE_SESSION_KEY in request.session:
                 del request.session[translation.LANGUAGE_SESSION_KEY] #get language from browser
 
+            if request.POST.get('checkbox_corpor_mode_train') and request.POST.get('train_corpor_select_post') and request.POST.get('train_corbooker_select_post'):
+                updated_request = request.POST.copy()
+                updated_request.update({
+                    'customer_parent_seq_id': request.POST['train_corpor_select_post']
+                })
+                cur_session = request.session['user_account']
+                cur_session.update({
+                    "co_customer_parent_seq_id": request.POST['train_corpor_select_post'],
+                    "co_customer_seq_id": request.POST['train_corbooker_select_post']
+                })
+                set_session(request, 'user_account', cur_session)
+                activate_corporate_mode(request, request.session['signature'])
+
             values.update({
                 'static_path': path_util.get_static_path(MODEL_NAME),
                 'titles': ['MR', 'MRS', 'MS', 'MSTR', 'MISS'],
