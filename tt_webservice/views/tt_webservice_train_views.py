c4528e66c31186d53c84f4bbeb88944ca08a304a
ivan <ivan@rodextravel.tours>
2023-11-01 13:53:24 +0700

    update_otp_params

--- tt_webservice/views/tt_webservice_train_views.py
+++ tt_webservice/views/tt_webservice_train_views.py
@@ -108,14 +108,17 @@ def login(request):
             "co_password": request.session.get('password') or password_default,
             "co_uid": ""
         }
+        otp_params = {}
         if request.POST.get('unique_id'):
-            data['machine_code'] = request.POST['unique_id']
+            otp_params['machine_code'] = request.POST['unique_id']
         if request.POST.get('platform'):
-            data['platform'] = request.POST['platform']
+            otp_params['platform'] = request.POST['platform']
         if request.POST.get('browser'):
-            data['browser'] = request.POST['browser']
+            otp_params['browser'] = request.POST['browser']
         if request.POST.get('timezone'):
-            data['timezone'] = request.POST['timezone']
+            otp_params['timezone'] = request.POST['timezone']
+        if otp_params:
+            data['otp_params'] = otp_params
         headers = {
             "Accept": "application/json,text/html,application/xml",
             "Content-Type": "application/json",
