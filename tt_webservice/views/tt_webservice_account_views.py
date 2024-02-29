e9ba09002f630c9817e699df4332cce66b7193d9
ivan <ivan@rodextravel.tours>
2024-02-29 10:09:50 +0700

    update_copy_domain

--- tt_webservice/views/tt_webservice_account_views.py
+++ tt_webservice/views/tt_webservice_account_views.py
@@ -1208,7 +1208,8 @@ def set_payment_partner(request):
                     if counter != 1:
                         title += str(counter)
                     break
-            text = request.POST['state'] + '\n' + sequence + '\n' + title + '\n' + fs.base_url + request.META['HTTP_HOST'].split(':')[0] + "/image_payment_partner/" + filename
+            domain = get_domain_cache(request)
+            text = request.POST['state'] + '\n' + sequence + '\n' + title + '\n' + fs.base_url + domain + "/image_payment_partner/" + filename
             write_cache(text, "".join(title.split(' ')), request, "payment_partner")
         #replace
         else:
@@ -1233,7 +1234,8 @@ def set_payment_partner(request):
                     if counter != 1:
                         title += str(counter)
                     break
-            text = request.POST['state'] + '\n' + sequence + '\n' + title + '\n' + fs.base_url + request.META['HTTP_HOST'].split(':')[0] + "/image_payment_partner/" + filename
+            domain = get_domain_cache(request)
+            text = request.POST['state'] + '\n' + sequence + '\n' + title + '\n' + fs.base_url + domain + "/image_payment_partner/" + filename
             write_cache(text, "".join(title.split(' ')), request, "payment_partner")
         #check image
         data = os.listdir(path)
@@ -1407,7 +1409,8 @@ def set_about_us(request):
                     if counter != 1:
                         sequence += str(counter)
                     break
-            text = request.POST['state'] + '\n' + sequence + '\n' + title + '\n' + body + '\n' + fs.base_url + request.META['HTTP_HOST'].split(':')[0] + "/image_about_us/" + filename
+            domain = get_domain_cache(request)
+            text = request.POST['state'] + '\n' + sequence + '\n' + title + '\n' + body + '\n' + fs.base_url + domain + "/image_about_us/" + filename
             write_cache(text, "".join(sequence.split(' ')), request, "about_us")
         #replace
         else:
@@ -1436,7 +1439,8 @@ def set_about_us(request):
                     if counter != 1:
                         sequence += str(counter)
                     break
-            text = request.POST['state'] + '\n' + sequence + '\n' + title + '\n' + body + '\n' + fs.base_url + request.META['HTTP_HOST'].split(':')[0] + "/image_about_us/" + filename
+            domain = get_domain_cache(request)
+            text = request.POST['state'] + '\n' + sequence + '\n' + title + '\n' + body + '\n' + fs.base_url + domain + "/image_about_us/" + filename
             write_cache(text, "".join(sequence.split(' ')), request, "about_us")
         #check image
         data = os.listdir(path)
